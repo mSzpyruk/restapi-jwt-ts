43715a04b79368a0f1ef314305ea7ba65303c0e6
mSzpyruk <michal.szpyruk@gmail.com>
2021-02-11 16:53:38 +0100

    DB setup.

---
+++ src/config/db-config.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+export default {
+  jwtSecret: process.env.JWT_SECRET || 'secrettoken',
+  DB: {
+    URI: process.env.MONGODB_URI,
+    USER: process.env.MONGODB_USER,
+    PASSWORD: process.env.MONGODB_PASSWORD,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from './config/app';
+import './config/database';
 
 app.listen(app.get('port'));
 console.log(`Listening on port:${app.get('port')}`);
