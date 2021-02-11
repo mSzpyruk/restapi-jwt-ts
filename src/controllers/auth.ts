cb7252fdf65d336b84f175dad503e8c74a205909
mSzpyruk <michal.szpyruk@gmail.com>
2021-02-11 20:53:31 +0100

    Create new user with hashed password.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
+import User, { UserProps } from '../models/User';
 
 export const register = async (req: Request, res: Response) => {
-  res.send('register');
+  const { username, email, password } = req.body;
+  const newUser: UserProps = new User({ username, email, password });
+  const user = await newUser.save();
+  console.log(user)
 };
 
 export const login = async (req: Request, res: Response) => {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 import { model, Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export interface UserProps extends Document {
   username: string;
   email: string;
   password: string;
   createdAt: Date;
+  comparePassword: (password: string) => Promise<Boolean>;
 }
 
 const userSchema = new Schema<UserProps>({
@@ -30,5 +32,21 @@ const userSchema = new Schema<UserProps>({
     default: Date.now,
   },
 });
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+  user.password = hashedPassword;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
 
 export default model('User', userSchema);
